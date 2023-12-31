f61db6699bfbe7c4a308db5f7c9b61cc5a2a3494
Yash <yash@github.com>
2023-12-31 19:13:52 +0530

    Adding Not Found Page

--- App.js
+++ App.js
@@ -3,44 +3,45 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 
-import { HomeScreen as HomeScreen001 } from './screens/day001/HomeScreen';
-import { SignInScreen as SignInScreen001 } from './screens/day001/SignInScreen';
-import { SignUpScreen as SignUpScreen001 } from './screens/day001/SignUpScreen';
+// import { HomeScreen as HomeScreen001 } from './screens/day001/HomeScreen.jsx';
+// import { SignInScreen as SignInScreen001 } from './screens/day001/SignInScreen.jsx';
+// import { SignUpScreen as SignUpScreen001 } from './screens/day001/SignUpScreen.jsx';
 
 
-import Home from "./screens/Home";
-import Details from "./screens/Details";
+
+// import Home from "./screens/Home";
+// import Details from "./screens/Details";
 
 
-const Day001Stack = createNativeStackNavigator();
-const Day001StackScreen = () => (
-  <Day001Stack.Navigator>
-    <Day001Stack.Screen name='Home' component={HomeScreen001} options={{ headerShown: false }} />
-    <Day001Stack.Screen name='SignUp' component={SignUpScreen001} options={{ headerShown: false }} />
-    <Day001Stack.Screen name='SignIn' component={SignInScreen001} options={{ headerShown: false }} />
-  </Day001Stack.Navigator>
-);
+// const Day001Stack = createNativeStackNavigator();
+// const Day001StackScreen = () => (
+//   <Day001Stack.Navigator>
+//     <Day001Stack.Screen name='Home' component={HomeScreen001} options={{ headerShown: false }} />
+//     <Day001Stack.Screen name='SignUp' component={SignUpScreen001} options={{ headerShown: false }} />
+//     <Day001Stack.Screen name='SignIn' component={SignInScreen001} options={{ headerShown: false }} />
+//   </Day001Stack.Navigator>
+// );
 
-const HomeStack = createNativeStackNavigator();
-const HomeStackScreen = () => (
-  <HomeStack.Navigator>
-    <HomeStack.Screen name='Home' component={Home} options={{ headerShown: false }} />
-    <HomeStack.Screen name='Details' component={Details} options={{ headerShown: false }} />
-  </HomeStack.Navigator>
-);
+// const HomeStack = createNativeStackNavigator();
+// const HomeStackScreen = () => (
+//   <HomeStack.Navigator>
+//     <HomeStack.Screen name='Home' component={Home} options={{ headerShown: false }} />
+//     <HomeStack.Screen name='Details' component={Details} options={{ headerShown: false }} />
+//   </HomeStack.Navigator>
+// );
 
-const RootStack = createNativeStackNavigator();
-const RootStackScreen = () => (
-  <RootStack.Navigator initialRouteName={'Day001'}>
-    <RootStack.Screen name='Home' component={HomeStackScreen} options={{ headerShown: false }} />
-    <RootStack.Screen name='Day001' component={Day001StackScreen} options={{ headerShown: false }} />
+// const RootStack = createNativeStackNavigator();
+// const RootStackScreen = () => (
+//   <RootStack.Navigator initialRouteName={'Day001'}>
+//     <RootStack.Screen name='Home' component={HomeStackScreen} options={{ headerShown: false }} />
+//     <RootStack.Screen name='Day001' component={Day001StackScreen} options={{ headerShown: false }} />
 
-    </RootStack.Navigator>
-);
+//     </RootStack.Navigator>
+// );
 
 const theme = {
   ...DefaultTheme,
@@ -63,21 +64,21 @@ const App = () => {
 
   if (!loaded) return null;
 
-  // return (
-  //   <NavigationContainer theme={theme}>
-  //     <Stack.Navigator
-  //       screenOptions={{
-  //         headerShown: false,
-  //       }}
-  //       initialRouteName="Home"
-  //     >
-  //       <Stack.Screen name="Home" component={Home} />
-  //       <Stack.Screen name="Home" component={Home} />
-  //       <Stack.Screen name="Details" component={Details} />
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
         
-  //     </Stack.Navigator>
-  //   </NavigationContainer>
-  // );
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 
   return (
     <SafeAreaProvider>
